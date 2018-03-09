36c328307422edd3051ffbbb6194450ed66f4437
thanapongnoomchunchombun <36655883+thanapongnoomchunchombun@users.noreply.github.com>
2018-03-09 14:27:54 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # basketofgrowserver
 # basketofgrowserver
+# basketofgrowserver
---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello world!'))
+app.listen(8080, () => console.log('Example to listen'))
